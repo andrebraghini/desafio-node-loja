856fb3bb0c77cc0978e7a304d3ef8515ae55ef0f
Andre Braghini <dedoxbr@gmail.com>
2021-01-29 11:05:30 -0300

    Controle principal com pontos de entrada da API

---
+++ src/ctrl/product.ctrl.ts
@@ -0,0 +1,99 @@
+import { Request, Response } from 'firebase-functions';
+import { Message } from 'firebase-functions/lib/providers/pubsub';
+import { onPubSubPublish, onRequest } from 'firebase-triggers';
+import { injectable, singleton } from 'tsyringe';
+import { ProductAddData, ProductAddPubSub } from '../pubsub/product-add.pubsub';
+import { ProductUpdateData, ProductUpdatePubSub } from '../pubsub/product-update.pubsub';
+import { ProductRemoveData, ProductRemovePubSub } from '../pubsub/product-remove.pubsub';
+import { ProductService } from '../services/product.service';
+
+@singleton()
+@injectable()
+export class ProductCtrl {
+
+    constructor(
+        private productService: ProductService,
+        private productAddPubSub: ProductAddPubSub,
+        private productUpdatePubSub: ProductUpdatePubSub,
+        private productRemovePubSub: ProductRemovePubSub,
+    ) {}
+
+    @onRequest({ path: 'products', methods: ['PUT', 'PATCH'] })
+    async rest(request: Request, response: Response) {
+        const id = request.path.split('/')[1];
+        const methods: any = {
+            'GET': id ? this.get : this.list,
+            'POST': this.post,
+            'PUT': this.put,
+            'PATCH': this.put,
+            'DELETE': this.del,
+        };
+        const method = methods[request.method];
+        return method(request, response);
+    }
+
+    private async post(request: Request, response: Response) {
+        await this.productAddPubSub.publish(request.body);
+        response.status(201).send();
+    }
+
+    private async put(request: Request, response: Response) {
+        const id = request.path.split('/')[1];
+        const partialUpdate = request.method === 'PATCH';
+        const data = { ...request.body, id, partialUpdate };
+        await this.productUpdatePubSub.publish(data);
+        response.status(204).send();
+    }
+
+    private async del(request: Request, response: Response) {
+        const id = request.path.split('/')[1];
+        await this.productRemovePubSub.publish({ id });
+        response.status(204).send();
+    }
+
+    @onRequest({ methods: 'GET' })
+    async get(request: Request, response: Response) {
+        const id = request.path.split('/')[1];
+        const product = id && await this.productService.get({ id });
+
+        if (!product && product.length) {
+            response
+                .status(404)
+                .json({
+                    success: false,
+                    msg: 'Product not found'
+                });
+        }
+
+        response.json(product[0]);
+    }
+
+    @onRequest({ methods: 'GET' })
+    async list(request: Request, response: Response) {
+        const products = await this.productService.get(request.query);
+        response.json(products);
+    }
+
+    @onPubSubPublish(ProductAddPubSub.topic)
+    async add(message: Message) {
+        const product = message.json as ProductAddData;
+        await this.productService.insert(product);
+    }
+
+    @onPubSubPublish(ProductUpdatePubSub.topic)
+    async update(message: Message) {
+        const data = message.json as ProductUpdateData;
+        const newData: any = { ...data };
+        delete newData.id;
+        delete newData.partialUpdate;
+
+        await this.productService.update(data.id, newData, data.partialUpdate);
+    }
+
+    @onPubSubPublish(ProductRemovePubSub.topic)
+    async remove(message: Message) {
+        const { id } = message.json as ProductRemoveData;
+        await this.productService.remove(id);
+    }
+
+}
--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,12 @@
-import * as functions from "firebase-functions";
+import 'reflect-metadata';
+import { getFirebaseFunctionListToExport } from 'firebase-triggers';
+import * as ctrlList from './ctrl';
 
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
+// Mencionado lista de controles para que a aplicação possa encontrá-los
+ctrlList;
 
-export const helloWorld = functions.https.onRequest((request, response) => {
-  functions.logger.info("Hello logs!", {structuredData: true});
-  response.send("Hello from Firebase!");
-});
+// Exportar métodos para o Firebase Functions
+const list = getFirebaseFunctionListToExport();
+for (const key in list) {
+    exports[key] = list[key];
+}
